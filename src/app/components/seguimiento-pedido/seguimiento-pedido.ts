1f0771985225aceb5151d0237a8a099b304cb5fa
Federico Fasani <fedefasani@gmail.com>
2025-10-14 14:41:19 -0300

    iniciar componente seguimiento

---
+++ src/app/components/seguimiento-pedido/seguimiento-pedido.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguimiento-pedido',
+  imports: [],
+  templateUrl: './seguimiento-pedido.html',
+  styleUrl: './seguimiento-pedido.scss'
+})
+export class SeguimientoPedido {
+
+}
