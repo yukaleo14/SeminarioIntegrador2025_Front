40eee9db7b064f817318a8326a2a825c187041e7
leop <yukaleo43@gmail.com>
2026-04-23 11:21:13 -0300

    correccion componente eliminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { SucursalConsultaComponent } from './sucursal-consulta/sucursal-consulta
 import { SeguimientoPedido } from './components/seguimiento-pedido/seguimiento-pedido';
 import { SeleccionUbicaciones } from './components/seleccion-ubicaciones/seleccion-ubicaciones';
 import { TablaPedidos } from './components/tabla-pedidos/tabla-pedidos';
-import { HacerPedido } from './hacer-pedido/hacer-pedido';
 import { ProductosPorCategoria } from './productos-por-categoria/productos-por-categoria';
 import { CarritoComponent } from './carrito-component/carrito-component';
 
@@ -21,7 +20,6 @@ export const routes: Routes = [
     {path:'seguimiento' ,component: SeguimientoPedido},
     {path:'seleccion-ubicacion' , component: SeleccionUbicaciones},
     {path: 'tabla-pedidos', component: TablaPedidos},
-    {path: 'hacer-pedido/:idEmpresa', component: HacerPedido},
     {path: 'categoria/:idCategoria/productos', component: ProductosPorCategoria},
     {path: 'carrito', component: CarritoComponent},
 
